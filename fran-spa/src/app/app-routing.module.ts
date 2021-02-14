a82107efd6708d7946da3172ed8c2b165d2ef8f0
Francisco Miguel DIaz Montes <fmdiazmontes87@gmail.com>
2021-02-14 13:23:15 +0100

    flexbonx mixin styles add and Componets tab1/2 working

--- fran-spa/src/app/app-routing.module.ts
+++ fran-spa/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { Tab1Component } from './tab1/tab1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Server } from 'http';
+import { Tab2Component } from './tab2/tab2.component';
+
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/tab_1', pathMatch: 'full' },
+  { path: '', redirectTo: '/tab1', pathMatch: 'full' },
+  { path: 'tab1', component: Tab1Component },
+  { path: 'tab2', component: Tab2Component },
 ];
 
 @NgModule({
