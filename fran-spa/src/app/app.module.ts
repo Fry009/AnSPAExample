f2aa7866210e772bc8cb89bfe0f06330fc8573a8
Francisco Miguel DIaz Montes <fmdiazmontes87@gmail.com>
2021-02-14 16:43:51 +0100

    fix scss route

--- fran-spa/src/app/app.module.ts
+++ fran-spa/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { Tab1Component } from './tab1/tab1.component';
 import { Tab2Component } from './tab2/tab2.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     Tab1Component,
-    Tab2Component
+    Tab2Component,
+
   ],
   imports: [
     BrowserModule,
