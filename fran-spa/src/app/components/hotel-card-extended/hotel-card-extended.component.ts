8bf0a39a6ff3de1f13b740978e7438b5d8e9af67
Francisco Miguel <fmdiazmontes87@gmail.com>
2021-02-21 20:17:20 +0100

    accordeon version 1

--- fran-spa/src/app/components/hotel-card-extended/hotel-card-extended.component.ts
+++ fran-spa/src/app/components/hotel-card-extended/hotel-card-extended.component.ts
@@ -10,6 +10,35 @@ export class HotelCardExtendedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.toggleAccordion();
   }
+
+
+  toggleAccordion(){
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  }
+
+
+
+
+
+
 
 }
